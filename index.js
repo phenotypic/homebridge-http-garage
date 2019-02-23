401b6084a83df53e9132fcc02271618220ab29b9
phenotypic <33377034+phenotypic@users.noreply.github.com>
2019-02-23 14:33:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Service, Characteristic;
 var request = require("request");
 
-module.exports = function(homebridge){
+module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   homebridge.registerAccessory("homebridge-http-garage", "GarageDoorOpener", GarageDoorOpener);
@@ -16,7 +16,7 @@ function GarageDoorOpener(log, config) {
   this.serial = config.serial || 'HTTP Serial Number';
 
   this.username = config.username || null;
-	this.password = config.password || null;
+  this.password = config.password || null;
   this.timeout = config.timeout || 5000;
   this.http_method = config.http_method || 'GET';
 
@@ -29,59 +29,63 @@ function GarageDoorOpener(log, config) {
   this.autoLock = config.autoLock || false;
   this.autoLockDelay = config.autoLockDelay || 10;
 
-  if(this.username != null && this.password != null){
+  if (this.username != null && this.password != null) {
     this.auth = {
-      user : this.username,
-      pass : this.password
+      user: this.username,
+      pass: this.password
     };
   }
 
   this.log(this.name);
 
-	this.service = new Service.GarageDoorOpener(this.name);
+  this.service = new Service.GarageDoorOpener(this.name);
 }
 
 GarageDoorOpener.prototype = {
 
-	identify: function(callback) {
-		this.log("Identify requested!");
-		callback();
-	},
+  identify: function(callback) {
+    this.log("Identify requested!");
+    callback();
+  },
 
-  _httpRequest: function (url, body, method, callback) {
-      request({
-          url: url,
-          body: body,
-          method: this.http_method,
-          timeout: this.timeout,
-          rejectUnauthorized: false,
-          auth: this.auth
+  _httpRequest: function(url, body, method, callback) {
+    request({
+        url: url,
+        body: body,
+        method: this.http_method,
+        timeout: this.timeout,
+        rejectUnauthorized: false,
+        auth: this.auth
       },
-          function (error, response, body) {
-              callback(error, response, body);
-          });
+      function(error, response, body) {
+        callback(error, response, body);
+      });
   },
 
   setTargetDoorState: function(value, callback) {
     this.log("[+] Setting targetDoorState to %s", value);
-    if (value == 1) { url = this.closeURL } else { url = this.openURL }
-    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
-        if (error) {
-          this.log("[!] Error setting targetDoorState: %s", error.message);
-					callback(error);
+    if (value == 1) {
+      url = this.closeURL;
+    } else {
+      url = this.openURL;
+    }
+    this._httpRequest(url, '', this.http_method, function(error, response, responseBody) {
+      if (error) {
+        this.log("[!] Error setting targetDoorState: %s", error.message);
+        callback(error);
+      } else {
+        if (value == 1) {
+          this.log("[*] Started closing");
+          this.simulateClose();
         } else {
-          if (value == 1) {
-            this.log("[*] Started closing");
-            this.simulateClose();
-          } else {
-            this.log("[*] Started opening");
-            if (this.autoLock) {
-              this.autoLockFunction();
-            }
-            this.simulateOpen();
+          this.log("[*] Started opening");
+          if (this.autoLock) {
+            this.autoLockFunction();
           }
-          callback();
+          this.simulateOpen();
         }
+        callback();
+      }
     }.bind(this));
   },
 
@@ -109,21 +113,21 @@ GarageDoorOpener.prototype = {
     }, this.autoLockDelay * 1000);
   },
 
-	getServices: function() {
+  getServices: function() {
 
     this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
     this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED);
 
-		this.informationService = new Service.AccessoryInformation();
+    this.informationService = new Service.AccessoryInformation();
     this.informationService
-		  .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-		  .setCharacteristic(Characteristic.Model, this.model)
-		  .setCharacteristic(Characteristic.SerialNumber, this.serial);
+      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+      .setCharacteristic(Characteristic.Model, this.model)
+      .setCharacteristic(Characteristic.SerialNumber, this.serial);
 
     this.service
-  		.getCharacteristic(Characteristic.TargetDoorState)
+      .getCharacteristic(Characteristic.TargetDoorState)
       .on('set', this.setTargetDoorState.bind(this));
 
-		return [this.informationService, this.service];
-	}
+    return [this.informationService, this.service];
+  }
 };
