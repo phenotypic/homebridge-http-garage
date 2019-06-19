a72030b941ccb1315afdbad05602ce06a553d7ac
phenotypic <33377034+phenotypic@users.noreply.github.com>
2019-06-19 22:44:48 +0200

    Update

--- index.js
+++ index.js
@@ -1,137 +1,137 @@
-var Service, Characteristic;
-const request = require('request');
+var Service, Characteristic
+const request = require('request')
 const packageJson = require('./package.json')
 
-module.exports = function(homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-http-garage', 'GarageDoorOpener', GarageDoorOpener);
-};
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-http-garage', 'GarageDoorOpener', GarageDoorOpener)
+}
 
-function GarageDoorOpener(log, config) {
-  this.log = log;
+function GarageDoorOpener (log, config) {
+  this.log = log
 
-  this.name = config.name;
+  this.name = config.name
 
-  this.manufacturer = config.manufacturer || packageJson.author.name;
-  this.serial = config.serial || packageJson.version;
-  this.model = config.model || packageJson.name;
-  this.firmware = config.firmware || packageJson.version;
+  this.manufacturer = config.manufacturer || packageJson.author.name
+  this.serial = config.serial || packageJson.version
+  this.model = config.model || packageJson.name
+  this.firmware = config.firmware || packageJson.version
 
-  this.username = config.username || null;
-  this.password = config.password || null;
-  this.timeout = config.timeout || 3000;
-  this.http_method = config.http_method || 'GET';
+  this.username = config.username || null
+  this.password = config.password || null
+  this.timeout = config.timeout || 3000
+  this.http_method = config.http_method || 'GET'
 
-  this.openURL = config.openURL;
-  this.closeURL = config.closeURL;
+  this.openURL = config.openURL
+  this.closeURL = config.closeURL
 
-  this.openTime = config.openTime || 5;
-  this.closeTime = config.closeTime || 5;
+  this.openTime = config.openTime || 5
+  this.closeTime = config.closeTime || 5
 
-  this.autoLock = config.autoLock || false;
-  this.autoLockDelay = config.autoLockDelay || 10;
+  this.autoLock = config.autoLock || false
+  this.autoLockDelay = config.autoLockDelay || 10
 
   if (this.username != null && this.password != null) {
     this.auth = {
       user: this.username,
       pass: this.password
-    };
+    }
   }
 
-  this.log(this.name);
+  this.log(this.name)
 
-  this.service = new Service.GarageDoorOpener(this.name);
+  this.service = new Service.GarageDoorOpener(this.name)
 }
 
 GarageDoorOpener.prototype = {
 
-  identify: function(callback) {
-    this.log('Identify requested!');
-    callback();
+  identify: function (callback) {
+    this.log('Identify requested!')
+    callback()
   },
 
-  _httpRequest: function(url, body, method, callback) {
+  _httpRequest: function (url, body, method, callback) {
     request({
-        url: url,
-        body: body,
-        method: this.http_method,
-        timeout: this.timeout,
-        rejectUnauthorized: false,
-        auth: this.auth
-      },
-      function(error, response, body) {
-        callback(error, response, body);
-      });
+      url: url,
+      body: body,
+      method: this.http_method,
+      timeout: this.timeout,
+      rejectUnauthorized: false,
+      auth: this.auth
+    },
+    function (error, response, body) {
+      callback(error, response, body)
+    })
   },
 
-  setTargetDoorState: function(value, callback) {
-    this.log('[+] Setting targetDoorState to %s', value);
+  setTargetDoorState: function (value, callback) {
+    var url
+    this.log('[+] Setting targetDoorState to %s', value)
     if (value === 1) {
-      url = this.closeURL;
+      url = this.closeURL
     } else {
-      url = this.openURL;
+      url = this.openURL
     }
-    this._httpRequest(url, '', this.http_method, function(error, response, responseBody) {
+    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
       if (error) {
-        this.log('[!] Error setting targetDoorState: %s', error.message);
-        callback(error);
+        this.log('[!] Error setting targetDoorState: %s', error.message)
+        callback(error)
       } else {
         if (value === 1) {
-          this.log('[*] Started closing');
-          this.simulateClose();
+          this.log('[*] Started closing')
+          this.simulateClose()
         } else {
-          this.log('[*] Started opening');
+          this.log('[*] Started opening')
           if (this.autoLock) {
-            this.autoLockFunction();
+            this.autoLockFunction()
           }
-          this.simulateOpen();
+          this.simulateOpen()
         }
-        callback();
+        callback()
       }
-    }.bind(this));
+    }.bind(this))
   },
 
-  simulateOpen: function() {
-    this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPENING);
+  simulateOpen: function () {
+    this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPENING)
     setTimeout(() => {
-      this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
-      this.log('[*] Finished opening');
-    }, this.openTime * 1000);
+      this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN)
+      this.log('[*] Finished opening')
+    }, this.openTime * 1000)
   },
 
-  simulateClose: function() {
-    this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSING);
+  simulateClose: function () {
+    this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSING)
     setTimeout(() => {
-      this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
-      this.log('[*] Finished closing');
-    }, this.closeTime * 1000);
+      this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED)
+      this.log('[*] Finished closing')
+    }, this.closeTime * 1000)
   },
 
-  autoLockFunction: function() {
-    this.log('[+] Waiting %s seconds for autolock', this.autoLockDelay);
+  autoLockFunction: function () {
+    this.log('[+] Waiting %s seconds for autolock', this.autoLockDelay)
     setTimeout(() => {
-      this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED);
-      this.log('[*] Autolocking');
-    }, this.autoLockDelay * 1000);
+      this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED)
+      this.log('[*] Autolocking')
+    }, this.autoLockDelay * 1000)
   },
 
-  getServices: function() {
+  getServices: function () {
+    this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED)
+    this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED)
 
-    this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
-    this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED);
-
-    this.informationService = new Service.AccessoryInformation();
+    this.informationService = new Service.AccessoryInformation()
     this.informationService
       .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
       .setCharacteristic(Characteristic.Model, this.model)
       .setCharacteristic(Characteristic.SerialNumber, this.serial)
-      .setCharacteristic(Characteristic.FirmwareRevision, this.firmware);
+      .setCharacteristic(Characteristic.FirmwareRevision, this.firmware)
 
     this.service
       .getCharacteristic(Characteristic.TargetDoorState)
-      .on('set', this.setTargetDoorState.bind(this));
+      .on('set', this.setTargetDoorState.bind(this))
 
-    return [this.informationService, this.service];
+    return [this.informationService, this.service]
   }
-};
+}
