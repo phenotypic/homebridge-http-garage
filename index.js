d56efa952b33f2789022e41d0dd1767e1e7bafa8
phenotypic <33377034+phenotypic@users.noreply.github.com>
2020-05-13 19:34:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function GarageDoorOpener (log, config) {
 
   this.username = config.username || null
   this.password = config.password || null
-  this.timeout = config.timeout || 3000
+  this.timeout = config.timeout || 5000
   this.http_method = config.http_method || 'GET'
 
   this.pollInterval = config.pollInterval || 120
