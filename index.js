eef77d8a9357af495b51213846448524e2db9554
phenotypic <33377034+phenotypic@users.noreply.github.com>
2021-12-31 15:50:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function GarageDoorOpener (log, config) {
   this.autoLock = config.autoLock || false
   this.autoLockDelay = config.autoLockDelay || 20
 
-  this.manufacturer = config.manufacturer || packageJson.author.name
+  this.manufacturer = config.manufacturer || packageJson.author
   this.serial = config.serial || this.apiroute
   this.model = config.model || packageJson.name
   this.firmware = config.firmware || packageJson.version
