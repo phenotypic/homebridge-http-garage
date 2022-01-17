2691a84810ca00fda91dd1edb1e3ed24bfea043b
phenotypic <33377034+phenotypic@users.noreply.github.com>
2022-01-17 23:35:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Service, Characteristic
+let Service, Characteristic
 const packageJson = require('./package.json')
 const request = require('request')
 
@@ -64,7 +64,7 @@ GarageDoorOpener.prototype = {
   },
 
   _getStatus: function (callback) {
-    var url = this.apiroute + '/status'
+    const url = this.apiroute + '/status'
     this.log.debug('Getting status: %s', url)
 
     this._httpRequest(url, '', 'GET', function (error, response, responseBody) {
@@ -75,7 +75,7 @@ GarageDoorOpener.prototype = {
       } else {
         this.log.debug('Device response: %s', responseBody)
         try {
-          var json = JSON.parse(responseBody)
+          const json = JSON.parse(responseBody)
           this.service.getCharacteristic(Characteristic.CurrentDoorState).updateValue(json.currentState)
           this.service.getCharacteristic(Characteristic.TargetDoorState).updateValue(json.currentState)
           this.log.debug('Updated state to: %s', json.currentState)
@@ -88,7 +88,7 @@ GarageDoorOpener.prototype = {
   },
 
   setTargetDoorState: function (value, callback) {
-    var url = this.apiroute + '/setState?value=' + value
+    const url = this.apiroute + '/setState?value=' + value
     this.log.debug('Setting state: %s', url)
 
     this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
